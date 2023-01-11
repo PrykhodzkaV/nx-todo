982ac2ef1c25a6b8f42695945b88c07a2ef6e03d
Volha Bychkova <volha_bychkova@epam.com>
2023-01-11 18:34:56 +0300

    fix: @env

--- libs/todo-feature-lib/src/lib/todo-feature-lib.tsx
+++ libs/todo-feature-lib/src/lib/todo-feature-lib.tsx
@@ -1,5 +1,5 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import {NX_API_URL} from "@env"
+import {NX_API_URL} from "@env";
 export const apolloClient = new ApolloClient({
   uri: NX_API_URL,
   cache: new InMemoryCache(),
